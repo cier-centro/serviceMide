41b7f2a1fccd3e4f70105f702a2432bcfd97cbec
Diego Armando Castellanos Sabogal <castsabproyectos@gmail.com>
2016-07-14 11:36:55 -0500

    Se actualiza el json.

--- index.php
+++ index.php
@@ -6,8 +6,62 @@
 <div ng-module="mideUniversidades" >
     <div ng-controller="mideUniversidadesController">
 
-        <button id="btnSearch" ng-click="search();">Buscar</button>
-        
+        <div id="dvSearchNameUniversities">
+            <label>Listado de universidades: </label>
+            <input type="text" id="nameUniversity" ng-model="fieldSearch.nameUniversity">
+            <button id="btnSearch" ng-click="searchByNameUniversities();">Buscar</button>
+        </div>
+
+        <div id="dvSearchOther">
+            <table class="tbSearchUniversities">
+
+                <tr>
+                    <td>
+                        <label>Acreditadas: </label>
+                        <select id="isAccredited" ng-model="fieldSearch.isAccredited">
+                            <option value="">-- Seleccione --</option>
+                            <option value="SI">IES acreditadas</option>
+                            <option value="NO">IES no acreditada</option>
+                        </select>
+                    </td>
+
+                    <td>
+                        <label>Tipo de institución: </label>
+                        <select id="typeUniversity" ng-model="fieldSearch.typeUniversity">
+                            <option value="">-- Seleccione --</option>
+                            <option value="UNIVERSIDAD">Universidad</option>
+                            <option value="INSTITUCION UNIVERSITARIA">Institución universitaria</option>
+                        </select>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <label>Sector: </label>
+                        <select id="sector" ng-model="fieldSearch.sector">
+                            <option value="">-- Seleccione --</option>
+                            <option value="OFICIAL">IES públicas</option>
+                            <option value="PRIVADA">IES no públicas</option>
+                        </select>
+                    </td>
+                    <td>
+                        <label>Grupo de clasificación: </label>
+                        <select id="classificationGroup" ng-model="fieldSearch.classificationGroup">
+                            <option value="">-- Seleccione --</option> 
+                            <option value="Pregrado">Pregrado</option>
+                            <option value="Maestria">Maestria</option>
+                            <option value="Doctorado">Doctorado</option>
+                            <option value="Especializadas">Especializadas</option>
+                        </select>
+                    </td>
+                </tr>
+
+            </table>
+
+            <button id="btnSearch" ng-click="searchByOtherFields();">Buscar</button>
+
+        </div>
+
         <table class="tbUniversities">
             <thead>
                 <tr>
@@ -17,18 +71,18 @@
                     <th>Tipo de Institución</th>
                     <th>Acreditación</th>
                 </tr>
-            
+
                 <tr>
                     <th></th>
-                    <th><input ng-model="filters.nameUniversity"></th>
-                    <th><input ng-model="filters.sector"></th>
-                    <th><input ng-model="filters.typeUniversity"></th>
-                    <th><input ng-model="filters.isAccredited"></th>
+                    <th><input type="text" ng-model="filters.nameUniversity"></th>
+                    <th><input type="text" ng-model="filters.sector"></th>
+                    <th><input type="text" ng-model="filters.typeUniversity"></th>
+                    <th><input type="text" ng-model="filters.isAccredited"></th>
                 </tr>
             </thead>
-            
+
             <tbody>
-                <tr ng-repeat="arrayUniversities in universities | filter:filters">
+                <tr ng-repeat="arrayUniversities in universities| filter:filters">
                     <td>{{arrayUniversities.position}}</td>
                     <td>{{arrayUniversities.nameUniversity}}</td>
                     <td>{{arrayUniversities.sector}}</td>
@@ -37,7 +91,7 @@
                 </tr>
             </tbody>
         </table>
-    
+
     </div>
 </div>
 
--- js/mideUniversidadesController.js
+++ js/mideUniversidadesController.js
@@ -24,5 +24,109 @@ app.controller('mideUniversidadesController', function($scope, $http) {
         });
     };
     
+    $scope.searchByOtherFields = function() {
+        
+        var obj = {content: null};
+        var filterSearch = 0;
+        var arrayObject = '';
+        var numberFilterActive = 0;
+        
+        $scope.universities = [];
+        
+        $http.get('https://dl.dropboxusercontent.com/u/575652037/mide/edu-mide/resources/base-mide.json').success(function(data) {
+            
+            obj.content = data;
+            
+            var isAccredited = document.getElementById('isAccredited').value;
+            var typeUniversity = document.getElementById('typeUniversity').value;
+            var sector = document.getElementById('sector').value;
+            var classificationGroup = document.getElementById('classificationGroup').value;
+            
+            if(isAccredited == "" && typeUniversity == "" && sector == "" && classificationGroup == ""){
+                alert("Favor seleccione al menos un criterio de busqueda.");
+                return false;
+            }
+            
+            if(isAccredited)
+                numberFilterActive += 1;
+            if(typeUniversity)
+                numberFilterActive += 1;
+            if(sector)
+                numberFilterActive += 1;
+            if(classificationGroup)
+                numberFilterActive += 1;
+            
+            angular.forEach(obj.content, function(mide) {
+                filterSearch = 0;
+                arrayObject = {
+                    position: mide.position,
+                    nameUniversity: mide.nameUniversity,
+                    sector: mide.sector,
+                    typeUniversity: mide.typeUniversity,
+                    isAccredited: mide.isAccredited
+                };
+                
+                if(isAccredited){
+                    if(isAccredited == mide.isAccredited)
+                        filterSearch += 1;
+                }
+                
+                if(typeUniversity){
+                    if(typeUniversity == mide.typeUniversity)
+                        filterSearch += 1;
+                   }
+                
+                if(sector){
+                    if(sector == mide.sector)
+                        filterSearch += 1;
+                }
+                
+                if(classificationGroup){
+                    if(classificationGroup == mide.classificationGroup)
+                        filterSearch = 1;
+                }
+                
+                if(filterSearch == numberFilterActive){
+                    $scope.universities.push(arrayObject);
+                }
+                
+            });
+        });
+    };
+    
+    $scope.searchByNameUniversities = function() {
+        
+        var obj = {content: null};
+        var arrayObject = '';
+        
+        $scope.universities = [];
+        
+        $http.get('https://dl.dropboxusercontent.com/u/575652037/mide/edu-mide/resources/base-mide.json').success(function(data) {
+            
+            obj.content = data;
+            
+            var nameUniversity = document.getElementById('nameUniversity').value;
+            
+            if(nameUniversity == ""){
+                alert("Favor seleccione al menos un criterio de busqueda.");
+                return false;
+            }
+            
+            angular.forEach(obj.content, function(mide) {
+                
+                arrayObject = {
+                    position: mide.position,
+                    nameUniversity: mide.nameUniversity,
+                    sector: mide.sector,
+                    typeUniversity: mide.typeUniversity,
+                    isAccredited: mide.isAccredited
+                };
+                
+                if (mide.nameUniversity.indexOf(nameUniversity) > -1 || mide.nameUniversity.toUpperCase().indexOf(nameUniversity.toUpperCase()) > -1)
+                    $scope.universities.push(arrayObject);
+                
+            });
+        });
+    };
+    
 });
-
